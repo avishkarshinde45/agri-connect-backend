8a34e6ed8a5a1e3877ba82167a7446680fe64844
Avishkar Rajendra Shinde <avishkarshinde146@gmail.com>
2026-01-11 10:20:50 +0530

    Update decision.js

--- api/decision.js
+++ api/decision.js
@@ -1,29 +1,5 @@
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ error: "Only POST allowed" });
-//   }
-
-//   const { crop, city } = req.body || {};
-
-//   if (!crop || !city) {
-//     return res.status(400).json({
-//       error: "crop and city are required"
-//     });
-//   }
-
-//   return res.status(200).json({
-//     success: true,
-//     crop,
-//     city,
-//     recommendedTraders: [
-//       "Trader A",
-//       "Trader B",
-//       "Trader C"
-//     ]
-//   });
-// }
 export default function handler(req, res) {
-  return res.status(200).json({
+  res.status(200).json({
     message: "Backend alive"
   });
 }
